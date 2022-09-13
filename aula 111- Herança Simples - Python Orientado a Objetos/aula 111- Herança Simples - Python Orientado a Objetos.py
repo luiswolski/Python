cdc9f438443921ed343f4f129e0703b7bcbd7c72
Luis Wolski <lfwolskicorrea@gmail.com>
2022-09-13 20:23:15 -0300

    Create aula 111- Herança Simples - Python Orientado a Objetos.py

---
+++ aula 111- Herança Simples - Python Orientado a Objetos/aula 111- Herança Simples - Python Orientado a Objetos.py
@@ -0,0 +1,10 @@
+from classes import *
+
+c1 = Cliente('Luis', 22)
+c1.falar()
+c1.comprar()
+# O comando c1.estudar() nao funciona aqui, pois é referente a outra classe
+
+a1 = Aluno('Maria', 42)
+a1.falar()
+a1.estudar()
