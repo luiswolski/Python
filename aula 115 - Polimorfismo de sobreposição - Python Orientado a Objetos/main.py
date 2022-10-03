8f46120ce0ba0e108ca688d6a326be894fc670eb
Luis Wolski <lfwolskicorrea@gmail.com>
2022-10-03 19:30:33 -0300

    Initial commit

---
+++ aula 115 - Polimorfismo de sobreposição - Python Orientado a Objetos/main.py
@@ -0,0 +1,22 @@
+from abc import ABC, abstractmethod
+
+
+class A(ABC):
+    @abstractmethod
+    def fala(self, msg): pass
+
+
+class B(A):
+    def fala(self, msg):
+        print(f'B está falando {msg}')
+
+
+class C(A):
+    def fala(self, msg):
+        print(f'C está falando {msg}')
+
+
+b = B()
+c = C()
+b.fala('UM ASSUNTO')
+c.fala('OUTRO ASSUNTO')
