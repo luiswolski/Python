cbc6be4ae264c8cfb3902729fb1051b2b3b43c21
Luis Wolski <lfwolskicorrea@gmail.com>
2022-10-02 23:27:25 -0300

    02/10

---
+++ aula 112 - Sobreposição de membros - Python Orientado a Objetos/aula 112 - Sobreposição de membros - Python Orientado a Objetos.py
@@ -0,0 +1,9 @@
+from classes import *
+
+c1 = Cliente('Luis', 22)
+c1.comprar()
+
+print()
+
+c2 = ClienteVIP('Amanda', 27, 'Wolski')
+c2.falar()
---
+++ aula 112 - Sobreposição de membros - Python Orientado a Objetos/classes.py
@@ -0,0 +1,28 @@
+class Pessoa:
+    def __init__(self, nome, idade):
+        self.nome = nome
+        self.idade = idade
+        self.nomeclasse = self.__class__.__name__
+
+    def falar(self):
+        print(f'{self.nomeclasse} falando...')
+
+
+class Cliente(Pessoa):
+    def comprar(self):
+        print(f'{self.nomeclasse} comprando...')
+
+    def falar(self):
+        print('Estou em CLIENTE!')
+
+
+class ClienteVIP(Cliente):
+    def __init__(self, nome, idade, sobrenome):
+        Pessoa.__init__(self, nome, idade)
+        self.sobrenome = sobrenome
+
+    def falar(self):
+        Pessoa.falar(self)
+        Cliente.falar(self)
+        print(f'{self.nome} {self.sobrenome}')
+
