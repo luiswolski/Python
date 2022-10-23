fd429044690fa72f64714f873c8c9427c4f3c349
Luis Wolski <lfwolskicorrea@gmail.com>
2022-10-23 15:25:58 -0300

    Create main.py

---
+++ aula 132 - Datetime #2 - Datas em português/main.py
@@ -0,0 +1,16 @@
+from datetime import datetime
+from locale import setlocale, LC_ALL
+from calendar import mdays
+
+
+setlocale(LC_ALL, 'pt_BR.utf-8')
+
+dt = datetime.now()
+mes_atual = int(dt.strftime('%m'))
+formatado = dt.strftime('%A, %d de %B de %Y')
+print(formatado)
+
+print(mes_atual, mdays[mes_atual])
+
+
+
