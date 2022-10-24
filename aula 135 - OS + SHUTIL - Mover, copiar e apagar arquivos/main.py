421696051206a04979bd22a7b5abc5b477c91c42
Luis Wolski <lfwolskicorrea@gmail.com>
2022-10-23 21:21:12 -0300

    Update main.py

--- aula 135 - OS + SHUTIL - Mover, copiar e apagar arquivos/main.py
+++ aula 135 - OS + SHUTIL - Mover, copiar e apagar arquivos/main.py
@@ -10,4 +10,13 @@ try:
 except FileExistsError as e:
     print(f'Pasta {caminho_novo} já existe.')
 
-for root, dirs, files in
+for root, dirs, files in os.walk(caminho_novo):
+    for file in files:
+        old_file_path = os.path.join(root, file)
+        new_file_path = os.path.join(caminho_novo, file)
+        os.remove(new_file_path)
+
+# Como copiar arquivos
+#       shutil.copy( old_file_path, new_file_path)
+#       print(f'Arquivo {file} movido com sucesso.')
+
