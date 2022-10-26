a969168e186d59f5ad3133e14f464cde84b782d5
Luis Wolski <lfwolskicorrea@gmail.com>
2022-10-25 23:08:43 -0300

    Initial commit

---
+++ aula 143 - Sys,argv - Executando arquivos com argumentos no sistema/main.py
@@ -0,0 +1,29 @@
+#!usr/bin/env python3
+
+import sys
+import os
+
+argumento = sys.argv
+qtd_argumentos = len(argumento)
+
+if qtd_argumentos <= 1:
+    print('Faltando argumentos:')
+    print('-a', 'Para listar todos os arquivos nesta pasta', sep='\t')
+    print('-d', 'Para listar todos os diretórios nesta pasta', sep='\t')
+    sys.exit()
+
+so_arquivos = False
+if '-a' in argumento:
+    so_arquivos = True
+
+so_diretorios = False
+if '-d' in argumento:
+    so_diretorios = True
+
+for arquivo in os.listdir('.'):
+    if so_arquivos:
+        if os.path.isfile(arquivo):
+            print(arquivo)
+    if so_diretorios:
+        if os.path.isdir(arquivo):
+            print(arquivo)
