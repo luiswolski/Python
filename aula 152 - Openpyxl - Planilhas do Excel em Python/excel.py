e64465e730f955e36f232ce10744bdf8446f7da5
Luis Wolski <lfwolskicorrea@gmail.com>
2022-11-01 13:45:30 -0300

    Initial commit

--- aula 152 - Openpyxl - Planilhas do Excel em Python/excel.py
+++ aula 152 - Openpyxl - Planilhas do Excel em Python/excel.py
@@ -5,4 +5,18 @@ https://openpyxl.readthedocs.io/en/stable/
 pip install openpyxl
 """
 import openpyxl
+from random import uniform
 
+pedidos = openpyxl.load_workbook('pedidos.xlsx')
+nome_panilhas = pedidos.sheetnames
+planilha1 = pedidos['Página1']
+
+for linha in range(5, 10):
+    numero_pedido = linha - 1
+    planilha1.cell(linha, 1).value = numero_pedido
+    planilha1.cell(linha, 2).value = 1200 + linha
+
+    preco = round(uniform(10, 100), 2)
+    planilha1.cell(linha, 3).value = preco
+
+pedidos.save('nova_planilha.xlsx')
