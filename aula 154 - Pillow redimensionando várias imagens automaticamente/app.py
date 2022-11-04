c0bc1a8b976bb8835b9e0f8ee475a6a5174d9bfa
Luis Wolski <lfwolskicorrea@gmail.com>
2022-11-03 23:36:41 -0300

    Initial commit

--- aula 154 - Pillow redimensionando várias imagens automaticamente/app.py
+++ aula 154 - Pillow redimensionando várias imagens automaticamente/app.py
@@ -6,32 +6,55 @@ def main(main_images_folder, new_width=800):
     if not os.path.isdir(main_images_folder):
         raise NotADirectoryError(f'{main_images_folder} não existe!')
 
-# USANDO OS PARA DEFINIR O CAMINHO DAS IMAGENS
+    # USANDO OS PARA DEFINIR O CAMINHO DAS IMAGENS
 
     for root, dirs, files in os.walk(main_images_folder):
         for file in files:
             file_full_path = os.path.join(root, file)
             file_name, extension = os.path.splitext(file)
 
-# RENOMEANDO AS IMAGENS COMO _CONVERTED, E UTILIZADO O MESMO CAMINHO PARA SALVAR
+            # RENOMEANDO AS IMAGENS COMO _CONVERTED, E UTILIZADO O MESMO CAMINHO PARA SALVAR
             converted_tag = '_CONVERTED'
 
             new_file = file_name + converted_tag + extension
             new_file_full_path = os.path.join(root, new_file)
 
-# CHECANDO SE A IMAGEN JÁ FOI CONVERTIDA
+            """
+            # CASO VOCÊ QUEIRA REMOVER AS IMAGENS        
+             if converted_tag in file_full_path:
+                            os.remove(file_full_path)
+            """
+
+            if os.path.isfile(new_file_full_path):
+                print(f'Arquivo {new_file_full_path} já existe!')
+                continue
+
+            # CHECANDO SE A IMAGEN JÁ FOI CONVERTIDA
             if converted_tag in file_full_path:
                 continue
 
-# UTILIZANDO OS VALORES DE TAMANHO PARA DEFINIR OS NOVOS TAMANHOS DE IMAGEM
+            # UTILIZANDO OS VALORES DE TAMANHO PARA DEFINIR OS NOVOS TAMANHOS DE IMAGEM
             img_pillow = Image.open(file_full_path)
             width, height = img_pillow.size
-            new_height = round(new_width * height/width)
-            print(width, new_width, height, new_height)
+            new_height = round(new_width * height / width)
 
+            # AGORA DE FATO SERÁ FEITO O REDIMENSIONAMENTO
+            new_image = img_pillow.resize((new_width, new_height),
+                                          Image.LANCZOS
+                                          )
+
+            new_image.save(
+                new_file_full_path,
+                optmize=True,
+                quality=60,
+                exif=img_pillow.info['exif']  # MANTÉM OS DADOS DAS FOTOS NAS CÓPIAS
+            )
+
+            print(f'{file_full_path} convertido com sucesso!')
+            new_image.close()
             img_pillow.close()
 
 
 if __name__ == '__main__':
     main_images_folder = r'D:\Fotos_test'
-    main(main_images_folder, new_width=1920)
+    main(main_images_folder, new_width=640)
