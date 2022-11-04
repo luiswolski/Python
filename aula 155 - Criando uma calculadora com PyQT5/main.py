96188b5b3e49351e119fe39483fc0211115a9375
Luis Wolski <lfwolskicorrea@gmail.com>
2022-11-04 20:22:35 -0300

    Initial commit

--- aula 155 - Criando uma calculadora com PyQT5/main.py
+++ aula 155 - Criando uma calculadora com PyQT5/main.py
@@ -12,13 +12,74 @@ class Calculadora(QMainWindow):
 
         self.display = QLineEdit()
         self.grid.addWidget(self.display, 0, 0, 1, 5)
-        self.setDisabled(True)
-        self.setStyleSheet(
+        self.display.setDisabled(True)
+        self.display.setStyleSheet(
             '*{background: white; color: #000; font-size: 30px;}'
         )
         self.display.setSizePolicy(QSizePolicy.Preferred, QSizePolicy.Expanding)
 
+        self.add_btn(QPushButton('7'), 1, 0, 1, 1)
+        self.add_btn(QPushButton('8'), 1, 1, 1, 1)
+        self.add_btn(QPushButton('9'), 1, 2, 1, 1)
+        self.add_btn(QPushButton('+'), 1, 3, 1, 1)
+        self.add_btn(
+            QPushButton('C'), 1, 4, 1, 1,
+            lambda: self.display.setText(''),
+            'background: #d5580d; color: #fff; font-weight: 700'
+        )
+
+        self.add_btn(QPushButton('4'), 2, 0, 1, 1)
+        self.add_btn(QPushButton('5'), 2, 1, 1, 1)
+        self.add_btn(QPushButton('6'), 2, 2, 1, 1)
+        self.add_btn(QPushButton('-'), 2, 3, 1, 1)
+        self.add_btn(
+            QPushButton('<-'), 2, 4, 1, 1,
+            lambda: self.display.setText(
+                self.display.text()[:-1]
+            ),
+            'background: #13823a; color: #fff; font-weight: 700'
+        )
+
+        self.add_btn(QPushButton('1'), 3, 0, 1, 1)
+        self.add_btn(QPushButton('2'), 3, 1, 1, 1)
+        self.add_btn(QPushButton('3'), 3, 2, 1, 1)
+        self.add_btn(QPushButton('/'), 3, 3, 1, 1)
+        self.add_btn(QPushButton(''), 3, 4, 1, 1)
+
+        self.add_btn(QPushButton('.'), 4, 0, 1, 1)
+        self.add_btn(QPushButton('0'), 4, 1, 1, 1)
+        self.add_btn(QPushButton(''), 4, 2, 1, 1)
+        self.add_btn(QPushButton('*'), 4, 3, 1, 1)
+        self.add_btn(
+            QPushButton('='), 4, 4, 1, 1,
+            self.eval_igual,
+            'background: #095177; color: #fff; font-weight: 700'
+        )
+
         self.setCentralWidget(self.cw)
+
+    def add_btn(self, btn, row, col, rowspan, colspan, funcao=None, style=None):
+        self.grid.addWidget(btn, row, col, rowspan, colspan)
+        if not funcao:
+            btn.clicked.connect(
+                lambda: self.display.setText(
+                    self.display.text() + btn.text()
+                )
+            )
+        else:
+            btn.clicked.connect(funcao)
+        if style:
+            btn.setStyleSheet(style)
+
+        btn.setSizePolicy(QSizePolicy.Preferred, QSizePolicy.Expanding)
+
+    def eval_igual(self):
+        try:
+            self.display.setText(
+                str(eval(self.display.text()))
+            )
+        except Exception as e:
+            self.display.setText('Conta Inválida.')
 
 
 if __name__ == '__main__':
