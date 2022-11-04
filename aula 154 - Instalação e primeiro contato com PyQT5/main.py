94310ea6d79362976e88dad3c29be9dc016cf689
Luis Wolski <lfwolskicorrea@gmail.com>
2022-11-04 19:31:36 -0300

    Initial commit

--- aula 154 - Instalação e primeiro contato com PyQT5/main.py
+++ aula 154 - Instalação e primeiro contato com PyQT5/main.py
@@ -5,6 +5,17 @@ from PyQt5.QtWidgets import QMainWindow, QApplication, QPushButton, QWidget, QGr
 class App(QMainWindow):
     def __init__(self, parent=None):
         super().__init__(parent)
+        self.cw = QWidget()
+        self.grid = QGridLayout(self.cw)
+
+# Criando os atributos de estilo do botãp
+        self.btn = QPushButton('Texto do Botão')
+        self.btn.setStyleSheet('font-size: 30px')
+        self.grid.addWidget(self.btn, 0, 0, 1, 1)
+
+        self.btn.clicked.connect(lambda: print('Hello word!'))
+
+        self.setCentralWidget(self.cw)      # Inicializando o botão
 
 
 if __name__ == '__main__':
