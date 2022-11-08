8633598608e365cd35deef96a6ab84bc5cb0b842
Luis Wolski <lfwolskicorrea@gmail.com>
2022-11-08 13:09:16 -0300

    Initial commit

---
+++ aula 251- Python sqlite3 + DB Browser for SQLite/main.py
@@ -0,0 +1,37 @@
+import sqlite3
+
+
+class AgendaDB:
+    def __init__(self, arquivo):
+        self.conn = sqlite3.connect(arquivo)
+        self.cursor = self.conn.cursor()
+
+    def inserir(self, nome, telefone):
+        consulta = 'INSERT INTO agenda (nome, telefone) VALUES (?, ?)'
+        self.cursor.execute(consulta, (nome, telefone))
+        self.conn.commit()
+
+    def editar(self, nome, telefone, id):
+        consulta = 'UPDATE agenda SET nome=?, telefone=?, WHERE id=?'
+        self.cursor.execute(consulta, (nome, telefone, id))
+        self.conn.commit()
+
+    def excluir(self, id):
+        consulta = 'DELETE FROM agenda WHERE id=?'
+        self.cursor.execute(consulta, (id,))
+        self.conn.commit()
+
+    def listar(self):
+        self.cursor.execute('SELECT * FROM agenda')
+
+        for linha in self.cursor.fetchall():
+            print(linha)
+
+    def fechar(self):
+        self.cursor.close()
+        self.conn.close()
+
+
+if __name__ == '__main__':
+    agenda = AgendaDB('agenda.db')
+    agenda.inserir('luis', '111111')
